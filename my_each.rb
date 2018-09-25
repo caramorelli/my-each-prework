6dce8de1cdc3642c3cb70be80175cdbb68f725f0
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 00:21:19 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,16 +1,16 @@
-def my_each(arr)
-  new_arr = []
-  idx = 0 
-  while idx < arr.length 
-    if block_given?
-      new_arr << yield(arr[idx])
-    else 
-      new_arr << arr[idx]
-    end 
-    idx += 1
-  end 
-  new_arr
-end
+# def my_each(arr)
+#   new_arr = []
+#   idx = 0 
+#   while idx < arr.length 
+#     if block_given?
+#       new_arr << yield(arr[idx])
+#     else 
+#       new_arr << arr[idx]
+#     end 
+#     idx += 1
+#   end 
+#   new_arr
+# end
 
 
 
