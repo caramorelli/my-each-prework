7900d27bd0622158035a4d7f25c217ebec64d5bd
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 00:12:37 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,11 @@
-def my_each(&blk)
-  # new_arr = []
+def my_each(arr)
+  new_arr = []
   # 0.upto(self.length - 1) { |idx| new_arr << yield(self[idx]) }
-  # new_arr
+  idx = 0 
+  while idx < arr.length 
+  end 
+  new_arr
+  
   
   
 end
